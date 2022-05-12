976c9f40e5c44355fac9f18aaa2ec49c266e40ec
Ricardo de Jesús Pineda Fuentes <richarddj_@outlook.com>
2022-05-12 00:28:21 -0600

    12/5/2022 00:27

---
+++ Registro/Registro/clRegistro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Registro
+{
+    internal class clRegistro : conexion
+    {
+        private static string grado;
+        private static string nombre;
+        private static string apellido;
+        private static string identidad;
+
+        public static string Grado { get => grado; set => grado = value; }
+        public static string Nombre { get => nombre; set => nombre = value; }
+        public static string Apellido { get => apellido; set => apellido = value; }
+        public static string Identidad { get => identidad; set => identidad = value; }
+
+        public void insertar() {
+            conexion con = new conexion();
+            try
+            {
+                abrircn();
+
+                //Este string es para validar que no haya sido registrado anteriormente
+                string consulta = "SELECT COUNT(*) FROM `estudiantes` WHERE documento = "+identidad+"";
+                MySqlCommand cmd = new MySqlCommand(consulta, cn);
+                int cons = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (cons == 0)
+                {
+                    string consulta1 = "INSERT INTO `estudiantes`(`grado`, `nombres`, `apellidos`, `documento`,`clave`) VALUES ('" + grado + "','" + nombre + "','" + apellido + "','" + identidad + "',' ')";
+                    MySqlCommand cmd1 = new MySqlCommand(consulta1, cn);
+                    cmd1.ExecuteNonQuery();
+                    MessageBox.Show("Votante registrado exitosamente","Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else {
+                    MessageBox.Show("Ya existe un usuario registrado");
+                }
+                close();
+                
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Se ha encontrado el error " + ex, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
